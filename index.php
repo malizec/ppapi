234e558397a0b9b683471afdc410bef24019d11b
Nenad Cvetkovic <nenadcv@gmail.com>
2017-07-29 18:51:24 +0200

    Added some style for paralax and styles for bootstrap top nav

--- index.php
+++ index.php
@@ -5,13 +5,31 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>PPApi</title>
-    <!-- Bootstrap CSS -->
- <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-  -->
+ 
   <link rel="stylesheet" href="public/css/style.css">
 
   </head>
 <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">
+        <img src="public/images/primerprogetto-logo.gif" alt="Primer Progetto">
+      </a>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+<!--           <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+ --><!--           <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+ -->        </ul>
+      </div>
+    </nav>
+
 
 <div class="bgimg-1">
   <div class="caption">
@@ -56,8 +74,16 @@
 
 	
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+
+
+<script src="public/js/jquery-3.2.1.min.js"></script>
+
+<script src="public/js/theter.min.js"></script>
+
+<script src="public/js/bootstrap.min.js"></script>
+
+ <script src="public/js/app.js"></script>
+
   </body>
 </html>
